bbaf45f21dab0671c8da588e09a54ee23be12a5a
alsoasnerd <alsoasnerd@gmail.com>
2025-02-19 13:33:36 -0300

    feat(vim): disable swapfiles

--- init.lua
+++ init.lua
@@ -109,6 +109,8 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+-- Disable swapfile
+vim.opt.swapfile = false
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
